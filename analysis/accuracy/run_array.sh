133bee8e700d89bb088e5d796f49ce52aa0b6850
brpetrucci <petrucci@iastate.edu>
2025-03-31 11:55:40 -0500

    Finished coverage sims.

---
+++ analysis/accuracy/run_array.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+for i in `seq 1 1`
+do
+printf "rep <- " >> analysis/accuracy/aux_$i.Rev
+echo $i >> analysis/accuracy/aux_$i.Rev
+rb simulation/accuracy/refs/ref_2.Rev analysis/accuracy/aux_$i.Rev analysis/accuracy/master.Rev
+rm analysis/accuracy/aux_$i.Rev
+done
